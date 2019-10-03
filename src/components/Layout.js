7be74f163a7d61861cdc6d482d8852ada873622c
verygoodmarketing <49660351+verygoodmarketing@users.noreply.github.com>
2019-10-03 07:36:34 -0500

    Fix viewport initial-scale written incorrectly
    The initial scale attribute was written as `initialScale=1.0`. Changed to the correct format `initial-scale=1`

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,7 +13,7 @@ export default class Body extends React.Component {
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
