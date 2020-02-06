204718882be271d33b64ccc426100cedf32e94ea
Brad Bodine <bbodine1@gmail.com>
2020-02-06 10:12:29 -0600

    adding react-messenger-chat

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,29 +3,51 @@ import {Helmet} from 'react-helmet-async';
 import _ from 'lodash';
 
 import {safePrefix} from '../utils';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 import Header from './Header';
 import Footer from './Footer';
 
 export default class Body extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <Helmet>
-                    <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
-                    <meta charSet="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <meta name="google" content="notranslate" />
-                    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet"/>
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
-                </Helmet>
-                <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
-                  <Header {...this.props} />
-                  <main id="content" className="site-content">
-                    {this.props.children}
-                  </main>
-                  <Footer {...this.props} />
-                </div>
-            </React.Fragment>
+          <React.Fragment>
+            <Helmet>
+              <title>
+                {_.get(this.props, 'pageContext.frontmatter.title') &&
+                  _.get(this.props, 'pageContext.frontmatter.title') + ' - '}
+                {_.get(this.props, 'pageContext.site.siteMetadata.title')}
+              </title>
+              <meta charSet="utf-8" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+              <meta name="google" content="notranslate" />
+              <link
+                href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i"
+                rel="stylesheet"
+              />
+              <link rel="stylesheet" href={safePrefix('assets/css/main.css')} />
+            </Helmet>
+            <MessengerCustomerChat
+              pageId="1042711662596982"
+              appId=""
+              htmlRef="<REF_STRING>"
+            />
+            <div
+              id="page"
+              className={
+                'site palette-' +
+                _.get(this.props, 'pageContext.site.siteMetadata.palette')
+              }
+            >
+              <Header {...this.props} />
+              <main id="content" className="site-content">
+                {this.props.children}
+              </main>
+              <Footer {...this.props} />
+            </div>
+          </React.Fragment>
         );
     }
 }
