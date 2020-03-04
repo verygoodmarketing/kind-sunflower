6748a6dbe30f0d49852ae4109ce55f4c7263c731
Brad Bodine <bbodine1@gmail.com>
2020-03-03 20:20:27 -0600

    add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,6 +15,14 @@ export default class Body extends React.Component {
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="google" content="notranslate" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#273b8b" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />          
+          
           <link
             href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i"
             rel="stylesheet"
