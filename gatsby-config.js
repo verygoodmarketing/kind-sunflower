838e0fb6f539d5f05affae92046d99545ddfa119
Brad Bodine <bbodine1@gmail.com>
2020-03-04 14:55:10 -0600

    Adding tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,60 @@
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: require('./site-metadata.json'),
-    plugins: [
-        `gatsby-plugin-react-helmet-async`,
-        `gatsby-source-data`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-stackbit-static-sass`,
-            options: {
-                inputFile: `${__dirname}/src/sass/main.scss`,
-                outputFile: `${__dirname}/public/assets/css/main.css`
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [`gatsby-remark-component`]
-            }
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {
-                
-            }
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-                menus: require('./src/data/menus.json'),
-            }
-        }
-    ]
+  pathPrefix: '/',
+  siteMetadata: require('./site-metadata.json'),
+  plugins: [
+    `gatsby-plugin-react-helmet-async`,
+    `gatsby-source-data`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-stackbit-static-sass`,
+      options: {
+        inputFile: `${__dirname}/src/sass/main.scss`,
+        outputFile: `${__dirname}/public/assets/css/main.css`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-component`]
+      }
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {}
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+        menus: require('./src/data/menus.json')
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-K23N768',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
+        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
+        // dataLayerName: 'YOUR_DATA_LAYER_NAME'
+      }
+    }
+  ]
 };
